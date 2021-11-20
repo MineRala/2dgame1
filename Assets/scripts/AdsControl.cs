b7691e3c0ff88516437083ea39e42e682200d418
mine rala <mine-rala@hotmail.com>
2021-11-20 21:36:11 +0300

    Added resume game

---
+++ Assets/scripts/AdsControl.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsControl : MonoBehaviour
+{
+#if UNITY_IOS
+    private string gameId="3988310";
+#elif UNITY_ANDROID
+    private string gameId="3988311";
+#endif
+    [SerializeField] bool _testMode = true;
+
+
+    void Start()
+    {
+        Advertisement.Initialize(gameId, _testMode);
+    }
+
+    public void InitializeAds()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+            Debug.Log("reklamlar yuklendi.");
+
+        }
+        else
+        {
+            Debug.Log("reklamlar yuklenemedi.");
+        }
+    }
+}
+
